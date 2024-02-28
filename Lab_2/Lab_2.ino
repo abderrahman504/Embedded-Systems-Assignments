70747736ed7ece30cbdb0147a00a8d49f19b33ff
abderrahman504 <rahmankhsherb@gmail.com>
2024-02-28 02:15:12 +0200

    Lab 2 finished

--- Lab_2/Lab_2.ino
+++ Lab_2/Lab_2.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+int in_pin = A5, led_pin = 9;
 
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+  Serial.begin(9800);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float in_percentage = analogRead(in_pin) / 1024.0;
+  Serial.println(in_percentage);
+  analogWrite(led_pin, 255*in_percentage);
 }
